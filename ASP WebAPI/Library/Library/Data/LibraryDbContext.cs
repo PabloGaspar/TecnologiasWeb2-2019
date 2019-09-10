92fbbddc57396eafbb6fd0224d9ba092e81b0dab
Internet <Internet@LABDOC3>
2019-09-10 14:48:34 -0400

    changes

--- ASP WebAPI/Library/Library/Data/LibraryDbContext.cs
+++ ASP WebAPI/Library/Library/Data/LibraryDbContext.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Library.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.Data
 {
-    public class LibraryDbContext
+    public class LibraryDbContext : DbContext
     {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<AuthorEntity>().ToTable("Authors");
+            modelBuilder.Entity<AuthorEntity>().HasMany(a => a.Books).WithOne(b => b.Author);
+            modelBuilder.Entity<BookEntity>().Property(a => a.Id).ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<BookEntity>().ToTable("Books");
+            modelBuilder.Entity<BookEntity>().Property(b => b.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<BookEntity>().HasOne(b => b.Author).WithMany(a => a.Books);
+        }
+
+        public DbSet<AuthorEntity> Authors { get; set; }
+        public DbSet<BookEntity> Books { get; set; }
     }
 }

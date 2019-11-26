edd94a837297b834c370405654a1bc8aecc057cd
PabloGaspar <pablo.romero.salinas@gmail.com>
2019-11-26 04:50:51 +0100

    added fetch example

---
+++ FetchExample/GamesWebApp/script.js
@@ -0,0 +1,55 @@
+
+//https://www.freecodecamp.org/news/a-practical-es6-guide-on-how-to-perform-http-requests-using-the-fetch-api-594c3d91a547/
+//document.querySelector("input[type=button]").addEventListener("click", fetchData);
+document.getElementById("fetchbtn").addEventListener("click", fetchData);
+document.getElementById("postbtn").addEventListener("click", postData);
+
+
+
+function fetchData(){
+    fetch('http://localhost:57891/api/values')
+    .then((res) => {
+        console.log('res:', res);
+        return res.json()})
+    .catch(error => console.error('Error:', error))
+    .then((json) => {
+        console.log('Success:', json)
+        
+        document.querySelector('body .container').innerHTML = '<ul>' + json.map(function (element) {
+            return '<li>' + element + '</li>';
+        }).join('') + '</ul>'
+    });
+}
+
+async function fetchDataAsync(){
+    try {
+        const response = await fetch('http://localhost:57891/api/values');
+        const json = await response.json();
+        document.querySelector('body .container').innerHTML = '<ul>' + json.map(function (element) {
+            return '<li>' + element + '</li>';
+        }).join('') + '</ul>'
+    } catch (error) {
+        console.log(error);
+    }
+    
+    
+}
+
+function postData (){
+    var url = 'http://localhost:57891/api/values';
+    //var data = {username: 'example'};
+    var data = 'example';
+    fetch(url, {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers:{
+        'Content-Type': 'application/json'
+    }
+    }).then((res) => {
+        return res.json()})
+    .catch(error => console.error('Error:', error))
+    .then((response) => {
+        console.log('Success:', response)
+    });
+}
+

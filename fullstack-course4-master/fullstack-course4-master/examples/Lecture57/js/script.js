b073d3dd2edc8eebe4904ca30a2e388f230889d3
PabloGaspar <pablo.romero.salinas@gmail.com>
2019-11-26 05:37:02 +0100

    async explained

--- fullstack-course4-master/fullstack-course4-master/examples/Lecture57/js/script.js
+++ fullstack-course4-master/fullstack-course4-master/examples/Lecture57/js/script.js
@@ -5,17 +5,21 @@ document.addEventListener("DOMContentLoaded",
     // Unobtrusive event binding
     document.querySelector("button")
       .addEventListener("click", function () {
-        
+        var self = this;
+        var name = '';
         // Call server to get the name
         $ajaxUtils
           .sendGetRequest("data/name.txt", 
             function (request) {
-              var name = request.responseText;
-
-              document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
+              name = request.responseText;
+              console.log('this',self);
             });
+            setTimeout(() => {
+               document.querySelector("#content")
+              .innerHTML = "<h2>Hello " + name + "!</h2>";
+            }, 2000);
 
+            
         
       });
   }
